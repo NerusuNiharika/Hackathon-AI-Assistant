c844995343802b5a1785f5ffe123b078013cdf6b
Nerusu Sai Niharika <nerusuniharika15@gmail.com>
2026-08-27 20:19:33 +0530

    Update cloud configuration

--- src/config.py
+++ src/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-
-QDRANT_URL = "http://localhost:6333"
+QDRANT_URL = os.getenv("QDRANT_URL")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
 COLLECTION_NAME = "hackathon_docs"
